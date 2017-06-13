91726976c20ffdec261cc06e4021466e294463bb
Eduardo Domene Junior <eduardo.djr@hotmail.com>
2017-06-13 10:38:43 -0300

    adicionado estado

--- player.js
+++ player.js
@@ -2,6 +2,7 @@ class Player {
   constructor(){
     this.playlist = ['song1.mp3'];
     this.currentSong = this.playlist[0];
+    this.state = 'STOPPED';
   }
   play(){
     console.log('Playing '+this.currentSong);
